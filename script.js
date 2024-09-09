189e8ed1ad6cca1d8765ef49120f73463b3202f0
Yuliya <yulka1998@yandex.ru>
2024-09-09 19:04:16 +0300

    add tasks from lesson 7

---
+++ script.js
@@ -0,0 +1,32 @@
+// Пример №1
+let a1 = 7 + 2;
+let a2 = 7 - 3;
+let a3 = 2 * 4;
+let a4 = 4 / 2;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+// Пример №2
+let height = 23;
+let width = 10;
+let s = height * width;
+console.log(`Площадь прямоугольника равна ${s} квадратных сантиметров`);
+
+// Пример №3
+let str = "qwerty";
+console.log(str[0], str[1], str[3]);
+
+// Пример №4
+console.log(`В одном часе ${1*60*60} секунд`);
+
+// Пример №5
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+console.log(num);
